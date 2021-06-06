037e11c9543bf9e039e47bc50fbaa80e41ad14bf
Asus\A s u s <masood_a_t@yahoo.com>
2021-06-07 01:21:56 +0400

    init commit

---
+++ Assets/Minsweeper/LevelGenerator.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelGenerator : MonoBehaviour, IuiView
+{
+    public GameObject BlockPrefab;
+    public List<block> Level;
+    public static LevelGenerator Instance;
+    public GameObject root;
+    public int Levelwidth;
+    public int Levelheihgt;
+    public int blocksize;
+    public int BombCount;
+    //public int BombsFound;
+    //public int SecondsPassed;
+    //float Timer;
+    public Button flagButton;
+    public bool Flagstate;
+    public Text bombsFoundText;
+    public Text TimeText;
+    public bool GameIsFinished;
+    public Text ResaultText;
+    public GameObject Resault;
+
+    public UIPresenter UIpresenter;
+    public List<BlockStateDatas> state;
+
+    public string flag { set => bombsFoundText.text = value; }
+    string IuiView.Timer { set => TimeText.text = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIpresenter = new UIPresenter(this, BombCount);
+        GameIsFinished = false;
+        Instance = this;
+        Level.Clear();
+        for (int i = 0; i < Levelheihgt; i++)
+        {
+            for (int j = 0; j < Levelwidth; j++)
+            {
+                GameObject b = Instantiate(BlockPrefab, Vector3.zero, Quaternion.identity);
+                b.transform.SetParent(root.transform);
+                b.transform.localPosition = new Vector3(j * blocksize, -i * blocksize, 0);
+                //b.GetComponent<block>().id = Level.Count;
+                //b.GetComponent<block>().hasMine = Random.Range(0, 100) > 80;
+                b.GetComponent<block>().Init(Level.Count,false);
+                Level.Add(b.GetComponent<block>());
+            }
+        }
+
+        for (int i = 0; i < BombCount; i++)
+        {
+            int id = Random.Range(0, Level.Count);
+            while (Level[id].blockPresenter.HasMine)
+                id = Random.Range(0, Level.Count);
+            Level[id].blockPresenter.HasMine = true;
+        }
+
+        root.transform.localPosition = new Vector3(
+            -(Levelwidth * blocksize / 2) + blocksize / 2,
+            (Levelwidth * blocksize / 2) - blocksize / 2,
+            0);
+
+        //Timer = 0;
+
+        //bombsFoundText.text = string.Format("Flag: {0}/{1}", BombsFound, BombCount);
+        UIpresenter.UpdateFlagCount(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Timer += Time.deltaTime;
+        //SecondsPassed = Mathf.FloorToInt(Timer);
+        UIpresenter.UpdateTimer();
+        //TimeText.text = "Timer : " + SecondsPassed.ToString();
+    }
+
+    public void restart()
+    {
+        SceneManager.LoadScene("Minsweeper");
+    }
+
+    public void SetFlag()
+    {
+        flagButton.interactable = false;
+        Flagstate = true;
+    }
+    public void ReleaseFlag()
+    {
+        flagButton.interactable = true;
+        Flagstate = false;
+    }
+
+    public void flagcount(int value)
+    {
+        //BombsFound += value;
+        //bombsFoundText.text = string.Format("Flag: {0}/{1}", BombsFound, BombCount);
+        UIpresenter.UpdateFlagCount(value);
+    }
+
+    public void MineExplude()
+    {
+        foreach (block bl in Level)
+            if (bl.blockPresenter.HasMine)
+                bl.Onclick();
+        GameIsFinished = true;
+        Showresault(true);
+    }
+
+    public void Showresault(bool explude)
+    {
+        Resault.SetActive(true);
+        ResaultText.text = (explude) ? "YOU LOSE!" : "YOU WIN!";
+    }
+    public void CheckAllCells()
+    {
+        bool allchecked = true;
+        foreach (block bl in Level)
+            if ((bl.blockPresenter.GetCurrentState() == BlockStateEnum.block)
+                || ((bl.blockPresenter.GetCurrentState() == BlockStateEnum.flagged) && !bl.blockPresenter.HasMine))
+                allchecked = false;
+
+        if (allchecked)
+            Showresault(false);
+    }
+}
+
---
+++ Assets/Minsweeper/block.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public enum BlockStateEnum { empty, block, flagged, mine, one, two, three, four, five, six, seven, eight }
+
+[System.Serializable]
+public class BlockStateDatas
+{
+    public BlockStateEnum state;
+    public Sprite image;
+}
+
+public class block : MonoBehaviour, IBlockView
+{
+    public Image image;
+    public BlockPresenter blockPresenter;
+
+    public Sprite sprite { set => image.sprite = value; }
+
+    public void Init(int _id, bool _hasbomb)
+    {
+        blockPresenter = new BlockPresenter(this, _id, _hasbomb);
+    }
+
+    public void Onclick()
+    {
+        blockPresenter.CheckState(LevelGenerator.Instance.Level);
+        LevelGenerator.Instance.CheckAllCells();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class BlockModel
+{
+    public int id;
+    public BlockStateEnum CurrentState;
+    public bool hasMine;
+
+    public BlockModel(int _id, bool _hasbomb)
+    {
+        CurrentState = BlockStateEnum.block;
+        id = _id;
+        hasMine = _hasbomb;
+    }
+
+    public void ChackState(int MineNear)
+    {
+        if (CurrentState == BlockStateEnum.empty)
+            return;
+        if (CurrentState == BlockStateEnum.mine)
+            return;
+
+
+        if (LevelGenerator.Instance.Flagstate)
+        {
+            LevelGenerator.Instance.ReleaseFlag();
+            if (CurrentState == BlockStateEnum.block)
+            {
+                CurrentState = BlockStateEnum.flagged;
+                LevelGenerator.Instance.flagcount(+1);
+                return;
+            }
+            else if (CurrentState == BlockStateEnum.flagged)
+            {
+                CurrentState = BlockStateEnum.block;
+                LevelGenerator.Instance.flagcount(-1);
+                return;
+            }
+
+        }
+
+        if (CurrentState == BlockStateEnum.flagged)
+            return;
+
+
+        if (hasMine)//explude
+        {
+            CurrentState = BlockStateEnum.mine;
+            //image.color = Color.red;
+            LevelGenerator.Instance.MineExplude();
+        }
+        else
+        {
+            if (MineNear == 0)//no mine
+            {
+                CurrentState = BlockStateEnum.empty;
+
+                int w = LevelGenerator.Instance.Levelwidth;
+                int length = LevelGenerator.Instance.Level.Count;
+                if ((id - w >= 0) && (id % w > 0)) LevelGenerator.Instance.Level[id - w - 1].Onclick(); //up left
+                if ((id - w >= 0)) LevelGenerator.Instance.Level[id - w].Onclick();//up
+                if ((id - w >= 0) && (id % w < w - 1)) LevelGenerator.Instance.Level[id - w + 1].Onclick();//up right
+                if ((id % w > 0)) LevelGenerator.Instance.Level[id - 1].Onclick();//left
+                if ((id % w < w - 1)) LevelGenerator.Instance.Level[id + 1].Onclick();//right
+                if ((id + w < length) && (id % w > 0)) LevelGenerator.Instance.Level[id + w - 1].Onclick();//down left
+                if ((id + w < length)) LevelGenerator.Instance.Level[id + w].Onclick();//down
+                if ((id + w < length) && (id % w < w - 1)) LevelGenerator.Instance.Level[id + w + 1].Onclick();//downright
+            }
+            else
+            {
+                CurrentState = (BlockStateEnum)MineNear + 3;
+            }
+        }
+
+    }
+
+    public int MinInNear(List<block> Allblocks)
+    {
+        int w = LevelGenerator.Instance.Levelwidth;
+        int length = Allblocks.Count;
+        int count = 0;
+        Debug.LogFormat("ul={0}  u={1}  ur={2}  l={3}  r={4}  dl={5}  d={6}  dr={7}    id={8}", id - w - 1, id - w, id - w + 1, id - 1, id + 1, id + w - 1, id + w, id + w + 1, id);
+
+        count += (id - w >= 0) && (id % w > 0) ? Allblocks[id - w - 1].blockPresenter.HasMine ? 1 : 0 : 0;//up left
+        count += (id - w >= 0) ? Allblocks[id - w].blockPresenter.HasMine ? 1 : 0 : 0;//up
+        count += (id - w >= 0) && (id % w < w - 1) ? Allblocks[id - w + 1].blockPresenter.HasMine ? 1 : 0 : 0;//up right
+        count += (id % w > 0) ? Allblocks[id - 1].blockPresenter.HasMine ? 1 : 0 : 0;//left
+        count += (id % w < w - 1) ? Allblocks[id + 1].blockPresenter.HasMine ? 1 : 0 : 0;//right
+        count += (id + w < length) && (id % w > 0) ? Allblocks[id + w - 1].blockPresenter.HasMine ? 1 : 0 : 0;//down left
+        count += (id + w < length) ? Allblocks[id + w].blockPresenter.HasMine ? 1 : 0 : 0;//down
+        count += (id + w < length) && (id % w < w - 1) ? Allblocks[id + w + 1].blockPresenter.HasMine ? 1 : 0 : 0;//downright
+        return count;
+    }
+}
+
+public interface IBlockView
+{
+    Sprite sprite { set; }
+}
+
+public class BlockPresenter
+{
+    BlockModel BlockModel;
+    private IBlockView blockView;
+
+
+    public bool HasMine
+    {
+        set => BlockModel.hasMine = value;
+        get { return BlockModel.hasMine; }
+    }
+
+    public int Id
+    {
+        get { return BlockModel.id; }
+    }
+
+    public BlockPresenter(IBlockView view, int _id, bool _hasbomb)
+    {
+        blockView = view;
+        BlockModel = new BlockModel(_id, _hasbomb);
+        updatestate();
+    }
+
+    public void CheckState(List<block> Allblocks)
+    {
+        int _MineNear = BlockModel.MinInNear(Allblocks);
+        BlockModel.ChackState(_MineNear);
+        updatestate();
+    }
+
+    public BlockStateEnum GetCurrentState()
+    {
+        return BlockModel.CurrentState;
+    }
+
+    public void updatestate()
+    {
+        blockView.sprite = LevelGenerator.Instance.state.Find(x => x.state == GetCurrentState()).image;
+    }
+}
