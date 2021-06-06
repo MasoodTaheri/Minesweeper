b44e3947a742e76ff8650c25ee8615aedd33f372
Asus\A s u s <masood_a_t@yahoo.com>
2021-06-07 01:22:54 +0400

    continue init commit

---
+++ Assets/Minsweeper/UI/UIModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIModel
+{
+    public int BombCount;
+    public int BombsFound;
+    //public int SecondsPassed;
+    float Timer;
+
+    public UIModel()
+    {
+        Timer = 0;
+    }
+
+    public string flagcount(int value)
+    {
+        BombsFound += value;
+        return string.Format("Flag: {0}/{1}", BombsFound, BombCount);
+    }
+
+    public string UpdateSecondpassed()
+    {
+        Timer += Time.deltaTime;
+        return "Timer : " + Mathf.FloorToInt(Timer).ToString();
+    }
+}
---
+++ Assets/Minsweeper/UI/UIPresenter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPresenter
+{
+    UIModel UIModel = new UIModel();
+    private IuiView uiview;
+
+    public UIPresenter(IuiView view, int bombcount)
+    {
+        uiview = view;
+        UIModel.BombCount = bombcount;
+    }
+
+    //public void ConnectBetweenModelAndView()
+    //{
+    //    UIModel.BombCount=
+    //}
+    public void UpdateTimer()
+    {
+        uiview.Timer = UIModel.UpdateSecondpassed();
+    }
+
+    public void UpdateFlagCount(int value)
+    {
+        uiview.flag = UIModel.flagcount(value);
+    }
+
+}
